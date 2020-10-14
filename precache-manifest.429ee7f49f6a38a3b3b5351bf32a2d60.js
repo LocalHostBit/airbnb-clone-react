d69467aa56a35d478c006a21c480376a28449707
LocalHostBit <growlyseo.com@gmail.com>
2020-10-14 12:46:14 +0200

    Updates

---
+++ precache-manifest.429ee7f49f6a38a3b3b5351bf32a2d60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "502d1d738819329a9524a0b1d5943253",
+    "url": "/airbnb-clone-react/index.html"
+  },
+  {
+    "revision": "27d4b60db0c6d8c78265",
+    "url": "/airbnb-clone-react/static/css/main.02c466ff.chunk.css"
+  },
+  {
+    "revision": "a9aa51900652ace47f5c",
+    "url": "/airbnb-clone-react/static/js/2.f49d4645.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/airbnb-clone-react/static/js/2.f49d4645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27d4b60db0c6d8c78265",
+    "url": "/airbnb-clone-react/static/js/main.794e1a48.chunk.js"
+  },
+  {
+    "revision": "6f9b9df3a133e65eca20",
+    "url": "/airbnb-clone-react/static/js/runtime-main.943116da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/airbnb-clone-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
